fdfbf7913bdf0a07aec41e01f174f77c63cd0414
Dušan Rosić SRB <149257819+dusanrsc@users.noreply.github.com>
2024-05-06 22:48:54 +0200

    serializers.py

---
+++ app/serializers.py
@@ -0,0 +1,9 @@
+# THIS IS ONLY EXAMPLE OF SERIALIZERS DO NOT HAVE ANY AFFECT ON THE PRODUCTION
+
+from rest_framework import serializers
+from .models import Event
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ["id", "title", "location", "description", "date", "time", "tickets"]
